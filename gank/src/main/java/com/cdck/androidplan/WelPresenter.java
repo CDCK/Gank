0bb083690413dc22096ac28ab4498b0cb818235d
CDCK <xlk214501580@163.com>
2019-03-09 17:54:29 +0800

    delete files

--- gank/src/main/java/com/cdck/androidplan/WelPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.cdck.androidplan;
-
-import com.cdck.androidplan.base.BasePresenter;
-
-/**
- * Created by xlk on 2019/3/5.
- */
-public class WelPresenter extends BasePresenter<WelContract.View> implements WelContract.Presenter {
-}
